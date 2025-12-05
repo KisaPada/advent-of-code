9c83d982bb3057a69167a9386881e7d3adfd0b71
KisaPada <jovan-3@hotmail.com>
2025-12-05 09:41:34 -0500

    day 02 complete

--- day02_part02.c
+++ day02_part02.c
@@ -4,13 +4,26 @@
 #define BUFSIZE 8192
 
 int isDupeStr(char *s) {
-    int isDupe = 0;
-
-    return isDupe;
+    int slen = strlen(s);
+    char subStr[32];
+    int subStrCount;
+    for (int subStrLen = 1; subStrLen <= slen / 2; subStrLen++) {
+        if ((slen % subStrLen) != 0) continue;
+        memset(subStr, 0, sizeof(subStr));
+        memcpy(subStr, s, subStrLen);
+        subStrCount = 0;
+        char *ptr = s;
+        while ((ptr = strstr(ptr, subStr))) {
+            subStrCount++;
+            ptr += subStrLen;
+        }
+        if ((subStrCount * subStrLen) == slen) return 1;
+    }
+    return 0;
 }
 
 int main(int argc, char **argv) {
-    char s[BUFSIZE] = {};
+    char s[BUFSIZE] = {0};
 
     FILE *f = fopen(argv[1], "r");
     if (!f) return perror("fopen"), -1;
@@ -21,8 +34,7 @@ int main(int argc, char **argv) {
     unsigned long long sum = 0;
 
     unsigned long long leftLim = 0, rightLim = 0;
-    char *lPtr = NULL, *mPtr = NULL, *rPtr = NULL;
-    char numStr[32] = {};
+    char numStr[32] = {0};
     for (char *tok = strtok(s, ","); tok != NULL; tok = strtok(NULL, ",")) {
         if (sscanf(tok, "%llu-%llu", &leftLim, &rightLim) != 2) {
             fprintf(stderr, "bad range\n");
@@ -39,7 +51,7 @@ int main(int argc, char **argv) {
         }
     }
 
-    printf("Part 01: %llu\n", sum);
+    printf("Part 02: %llu\n", sum);
 
     return 0;
 }
